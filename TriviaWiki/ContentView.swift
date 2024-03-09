d22dae2f6e4d81112af788b2f83cf19713fc0d56
Jake Young <umshmihle@gmail.com>
2024-03-08 16:02:48 -0800

    Initial Commit

---
+++ TriviaWiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriviaWiki
+//
+//  Created by Jake Young on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriviaWiki/TriviaWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaWikiApp.swift
+//  TriviaWiki
+//
+//  Created by Jake Young on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
