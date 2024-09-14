d6e1343881d44dc0f336877f30d55100e38d085f
Jake Young <jakeyoung@jakes-mbp.lan>
2024-09-13 21:22:54 -0700

    Added haptic feedback, changed app icon

---
+++ TriviaWiki/Views/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  TriviaWiki
+//
+//  Created by Jake Young on 9/13/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
---
+++ TriviaWiki/Views/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  TriviaWiki
+//
+//  Created by Jake Young on 9/13/24.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
