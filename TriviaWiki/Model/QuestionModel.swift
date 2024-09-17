2a1e8aeebfe242ef3859276d760cbfb7cd380586
Jake Young <jakeyoung@Jakes-MacBook-Pro.local>
2024-09-17 14:17:57 -0700

    removed old files

--- TriviaWiki/Model/QuestionModel.swift
+++ TriviaWiki/Model/QuestionModel.swift
@@ -8,50 +8,123 @@
 import Foundation
 import SwiftUI
 
-var example = Question()
+class QuestionFactory {
+    var questions : [Int: Question]
+    static var shared = QuestionFactory()
+    private init(){
+        questions = [Int:Question]()
+    }
+    func getQuestion(id: Int) -> Question{
+        if let foundQuestion = questions[id] {
+          return foundQuestion
+        } else {
+            let newQuestion = Question(id1: id)
+            questions[id] = newQuestion
+            return newQuestion
+            
+        }
+    }
+}
 
-class Question: ObservableObject{
+class Question: ObservableObject, Hashable {
+    static func == (lhs: Question, rhs: Question) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    func hash(into hasher: inout Hasher){
+        hasher.combine(ObjectIdentifier(self))
+    }
+    let ref = DataManager.shared
     let id: Int
     let link: String
     let question: String
     let options: [String]
-    let responseRates: [CGFloat]
+    let responseNums: [Int]
     let correctIndex: Int
-    let subject: String
-    let difficulty: Double
-    @Published var selectedIndex: Int
+    let topic : String
+    let quote : String
     @Published var complete: Bool
-    @Published var timeOnScreen: Double
-    @Published var clicks: Double
+    @Published var revealedHint: Bool
+    @Published var selectedIndex: Int?
+    @Published var liked: Bool?
+    @Published var report: String?
     
-    init(){
-    id = 0
-    link =  "https://en.wikipedia.org/wiki/Twitter"
-    question = "Who is the current CEO of twitter?"
-    options = ["Donald Trump","Elon Musk", "Darth Vader", "Neil Gaiman"]
-    responseRates = [0.50,0.25,0.125,0.125]
-    correctIndex = 1
-    subject = "business"
-    difficulty = 0.5
-    selectedIndex = 5
-    complete = false
-    timeOnScreen = 0.1
-    clicks = 0.0
-    }
-    init(id1:Int, topic:String, q: String, opts:[String], resps:[CGFloat], corr:Int, subj: String, diff: Double){
+//    init(){
+//        id = 0
+//        link =  "https://en.wikipedia.org/wiki/default"
+//        question = "This is the default question, if you are seeing this, there is a bug. Please report it in your profile"
+//        options = ["Cockroach","Fly", "Termite", "Beatle"]
+//        correctIndex = 1
+//        complete = false
+//        revealedHint = false
+//        responseNums = [0,0,0,0]
+//        topic = "default"
+//        quote = "In this example, defaultApp is the default Firebase app, and secondaryApp is another Firebase app connected to a different Firebase project. You can then use defaultDb and secondaryDb to interact with the respective Firestore databases."
+//    }
+    
+    init(id1 : Int){
         id = id1
-        link =  "https://en.wikipedia.org/wiki/" + topic.replacingOccurrences(of: " ", with: "_")
-        question = q
-        options = opts
-        responseRates = resps
-        correctIndex = corr
-        subject = subj
-        difficulty = diff
-        selectedIndex = 5
-        complete = false
-        timeOnScreen = 0.1
-        clicks = 0.0
+        link = ref.getQuestion_link(id: id1)
+        question = ref.getQuestion_question(id: id1)
+        options = ref.getQuestion_options(id: id1)
+        correctIndex = ref.getQuestion_correctIndex(id: id1)
+        complete = ref.getQuestion_complete(id: id1)
+        revealedHint = ref.getQuestion_revealedHint(id: id1)
+        responseNums = ref.getQuestion_responseNums(id: id1)
+        topic = ref.getQuestion_topic(id: id1)
+        quote = ref.getQuestion_quote(id: id1)
+        selectedIndex = ref.getQuestion_selectedIndex(id: id1)
+        liked = ref.getQuestion_liked(id: id1)
+        report = ref.getQuestion_report(id: id1)
     }
-    
-    
+
+    func like() -> Void {
+        if liked == true {
+            liked = nil
+        } else {
+            liked = true }
+        ref.seeQuestion(id: id)
+        return
+    }
+    func dislike() -> Void {
+        if liked == false {
+            liked = nil
+        } else {
+            liked = false }
+        ref.seeQuestion(id: id)
+        return
+    }
+    func reportQuestion(reason: String) -> Void {
+        ref.seeQuestion(id: id)
+        report = reason
+    }
+    func selectAnswer(selection: Int) -> Void {
+        ref.seeQuestion(id: id)
+        selectedIndex = selection
+        complete = true
+        ref.applyRatingChange(q: self)
+    }
+    func reveal() -> Void {
+        ref.seeQuestion(id: id)
+        revealedHint = true
+        ref.applyRatingChange(q: self)
+    }
+    func completedCorrectly() -> Bool {
+        return (selectedIndex == correctIndex) && (!revealedHint)
+    }
+    func serializeData() -> [String: Any]{
+        var result = [String: Any]()
+        result["id"] = id
+        if let liked1 = liked {
+            result["liked"] = liked1
+        }
+        if let report1 = report {
+            result["report"] = report1
+        }
+        if let selected = selectedIndex {
+            result["selectedIndex"] = selected
+        }
+        result["complete"] = complete
+        result["revealedHint"] = revealedHint
+        return result
+    }
 }
